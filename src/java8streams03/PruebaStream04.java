b1fecaa696597429165059318e14953294516274
programacion <72938653+ionicharnina2021@users.noreply.github.com>
2023-10-02 09:42:00 +0200

    mapeo a numericos

--- src/java8streams03/PruebaStream04.java
+++ src/java8streams03/PruebaStream04.java
@@ -2,10 +2,11 @@ package java8streams03;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PruebaStream04 {
 public static void main(String[] args) {
-	List<Integer> collect3 = Arrays.asList(9,4,5,4,3);
+	List<Integer> collect3 = Arrays.asList(9,4,5,4,3,9);
 	int primerRepetido=-1;
 	boolean encontrado=false;
 	for (int i = 0; i < collect3.size()-1&&!encontrado; i++) {
@@ -24,9 +25,18 @@ public static void main(String[] args) {
 	}
 	System.out.println("no streams "+primerRepetido);
 	System.out.println("con streams "+collect3.stream()
-		.filter((Integer elemento)->{
-			collect3.stream().anyMatch((otro)->{return elemento==otro;});
-		});
+		.filter((elemento)->{
+			return collect3.stream()
+					.filter((otro)->{return elemento==otro;})
+					.count()>1;})
+		.findFirst().orElse(-1));
+	System.out.println("con streams "+collect3.stream()
+	.filter((elemento)->{
+		return collect3.stream()
+				.filter((otro)->{return elemento==otro;})
+				.count()>1;
+	}).collect(Collectors.toList()));
+	// y si el primero que se repite es el 4 no el nueve. hazlo convencional y streams
 	
 	
 	
--- src/teoriastreamsDos05/PruebaStreamMetodos021.java
+++ src/teoriastreamsDos05/PruebaStreamMetodos021.java
@@ -2,7 +2,10 @@ package teoriastreamsDos05;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -37,9 +40,52 @@ public class PruebaStreamMetodos021 {
 		}).collect(Collectors.toList());
 		System.out.println(collect);
 		stream = cadenas.stream();
-		stream.noneMatch();
+		// ninguna empieza por x
+		System.out.println(stream.noneMatch((element) -> {
+			return element.startsWith("x");
+		}));
 		stream = cadenas.stream();
-		stream.takeWhile();
-
+		// mientras no empiece por a
+		System.out.println(stream.takeWhile((element) -> {
+			return !element.startsWith("a");
+		}).collect(Collectors.toList()));
 	}
+
+	// Estos necesitan un Consumer
+	Consumer<String> consumer = new Consumer<String>() {
+
+		@Override
+		public void accept(String t) {
+			// TODO Auto-generated method stub
+
+		}
+	};
+	// stream.forEachOrdered();
+	// stream.peek();
+	// Estos necesitan un Supplier
+	Supplier<String> supplier = new Supplier<String>() {
+
+		@Override
+		public String get() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	};
+	// Estos una funcion
+	Function<String, String> function = new Function() {
+
+		@Override
+		public Object apply(Object t) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	};
+	// stream.map();
+	// stream.flatMap();
+	// stream.flatMapToDouble();
+	// stream.flatMapToInt();
+	// stream.flatMapToLong();
+	// stream.mapToDouble();
+	// stream.mapToInt();
+	// stream.mapToLong();
 }
