7815168e2d338898262687e95824239c34434970
programacion <72938653+ionicharnina2021@users.noreply.github.com>
2023-10-04 13:34:28 +0200

    gropingby

--- src/ejercicio09/GatosService.java
+++ src/ejercicio09/GatosService.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GatosService {
 	private GatosRepository gatosRepository;
@@ -21,14 +22,36 @@ public class GatosService {
 			gatosRepository.addGato(gatoOM.getRandomCat());				
 		}while(gatosRepository.size()<maximoGatos);
 	}
-	public Map<Raza, Long> findCatsByRace(){
+	
+	public Map<Raza, Long> getAmountCatsByRaceStream(){
+		Map<Raza, Long> mapa = new HashMap();
+		Arrays.asList(Raza.values()).stream()
+		.forEach((raza)->{
+			mapa.put(raza,gatosRepository.findAll().stream()
+					.filter((gatete)->{return gatete.equalsRaza(raza);})
+					.count());
+		});
+		return mapa;
+	}
+	public Map<Raza, Long>  getAmountCatsByRaceOld(){
 		List<Raza> asList = Arrays.asList(Raza.values());
 		Map<Raza, Long> mapa = new HashMap();
 		for (Raza raza : asList) {
 			mapa.put(raza,gatosRepository.findAll().stream()
-					.filter((gatete)->{return gatete.equalsRaza(raza);}).count());
+					.filter((gatete)->{return gatete.equalsRaza(raza);})
+					.count());
 		}
 		return mapa;
 	}
 	
+	public Map<Raza,List<Gato>> getCatsByRaces(){
+		return gatosRepository.findAll().stream()
+		.collect(Collectors.groupingBy((gato)->{return gato.getRaza();}));
+		
+	}
+	public Map<Raza,Long> getAmountCatsByRacesGrouping(){
+		return gatosRepository.findAll().stream()
+				.collect(Collectors.groupingBy(
+						(gato)->{return gato.getRaza();},Collectors.counting()));
+	}
 }
--- src/ejercicio09/GatosServiceTest.java
+++ src/ejercicio09/GatosServiceTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.Map;
 
-import javax.xml.stream.util.EventReaderDelegate;
-
 import org.junit.jupiter.api.Test;
 
 class GatosServiceTest {
@@ -13,9 +11,16 @@ class GatosServiceTest {
 	@Test
 	void testFindCatsByRace() {
 		GatosService gatosService=new GatosService();
-		Map<Raza, Long> findCatsByRace = gatosService.findCatsByRace();
+		Map<Raza, Long> findCatsByRace = gatosService.getAmountCatsByRaceOld();
 		assertEquals(1000,findCatsByRace.entrySet().stream()
 			.mapToLong((entrada)->{return entrada.getValue();}).sum());
+		Map<Raza, Long> findCatsByRaceDos = gatosService.getAmountCatsByRaceStream();
+		assertEquals(1000,findCatsByRaceDos.entrySet().stream()
+			.mapToLong((entrada)->{return entrada.getValue();}).sum());
+		Map<Raza, Long> findCatsByRaceTres = gatosService.getAmountCatsByRacesGrouping();
+		assertEquals(1000,findCatsByRaceTres.entrySet().stream()
+				.mapToLong((entrada)->{return entrada.getValue();}).sum());
+		assertEquals(gatosService.getCatsByRaces().size(),Raza.values().length);
 	}
 
 }
